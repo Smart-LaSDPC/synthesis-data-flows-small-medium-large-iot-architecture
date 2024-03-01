354dab28a06027b05e6023ccceb84f7a70e230e5
Herminio-PC-LaSDPC <herminiopaucar@usp.br>
2024-03-01 20:39:41 -0300

    Data files and first plots

--- bibliometric-analise/plot_sourcesByYear.R
+++ bibliometric-analise/plot_sourcesByYear.R
@@ -18,7 +18,7 @@ ggplot(ssy, aes(x=year, y=total_count, color=source,
                  line=source, group= source))+ 
   scale_color_manual(values=c('#F4D03F','#3498DB','#E74C3C','#6C3483'))+
   geom_point(size=1,shape=21,aes(color=source))+
-  theme(axis.text.x = element_text(angle = 30, vjust = 0.5))+
+  theme(axis.text.x = element_text(angle = 0, vjust = 0.5))+
   scale_x_continuous("", labels = as.character(ssy$year), breaks = ssy$year)+
   scale_y_continuous("")+
   geom_line()
